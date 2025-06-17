8545fa2a6521fe2e59a936e48dd84d52fdb77270
jaysmito <jaysmito101@gmail.com>
2025-06-18 02:36:17 +0530

    chore: add the inputs to depth shader to avoid warnings

--- em_assets/shaders/scene_subsurface_scattering/SubSurfaceScatteringDepthFrag.glsl
+++ em_assets/shaders/scene_subsurface_scattering/SubSurfaceScatteringDepthFrag.glsl
@@ -3,6 +3,13 @@
 
 precision highp float;
 
+layout(location = 0) in vec2 inUV;
+layout(location = 1) in vec3 inNormal;
+layout(location = 2) in vec4 inPosition;
+layout(location = 3) flat in int inRenderingLight;
+// Extremely inefficient thing to do but well, this is just a demo.
+layout(location = 4) in vec3 inLightAPos;
+layout(location = 5) in vec3 inLightBPos;
 
 void main()
 {
