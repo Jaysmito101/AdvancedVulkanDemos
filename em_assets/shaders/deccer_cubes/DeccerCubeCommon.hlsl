d7332f3e7a16503ec55b6dd9c88f066c82b89e22
jaysmito <jaysmito101@gmail.com>
2025-09-21 04:06:26 +0530

    feat: remove redundant extensions from DeccerCubeCommon.hlsl

--- em_assets/shaders/deccer_cubes/DeccerCubeCommon.hlsl
+++ em_assets/shaders/deccer_cubes/DeccerCubeCommon.hlsl
@@ -1,8 +1,3 @@
-#extension GL_EXT_nonuniform_qualifier : require
-#extension GL_EXT_shader_16bit_storage : require
-#extension GL_EXT_shader_8bit_storage : require
-
-
 #include "MeshUtils"
 #include "MathUtils"
 #include "PBRUtils"
