4861bb482e37daf4fd980393c9b35a50b2eb2c09
Jaysmito101 <jaysmito101@gmail.com>
2025-12-13 00:59:55 +0530

    fix: change return type of __avdModelGltfFindAttribute to void

--- avd/src/model/avd_model_gltf_loader.c
+++ avd/src/model/avd_model_gltf_loader.c
@@ -49,7 +49,7 @@ static bool __avdModelLoadGltfTransform(AVD_Transform *transform, cgltf_node *no
     return true;
 }
 
-static cgltf_attribute *__avdModelGltfFindAttribute(cgltf_attribute *attributes, cgltf_size count, cgltf_attribute_type type, AVD_UInt32 index, void *scratchBuffer)
+static void *__avdModelGltfFindAttribute(cgltf_attribute *attributes, cgltf_size count, cgltf_attribute_type type, AVD_UInt32 index, void *scratchBuffer)
 {
     AVD_Size size = attributes[0].data->count;
     for (cgltf_size i = 0; i < count; i++) {
