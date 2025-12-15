77fa7feeeb960b34eed0f0779c9407b02a188265
Jaysmito101 <jaysmito101@gmail.com>
2025-12-15 21:51:42 +0530

    fix: update position indexing in vertex shader to prevent out-of-bounds access

--- em_assets/shaders/hls_player/HLSPlayerVert.hlsl
+++ em_assets/shaders/hls_player/HLSPlayerVert.hlsl
@@ -6,7 +6,7 @@ cbuffer PushConstants {
 };
 
 VertexShaderOutput main(uint vertexIndex : SV_VertexID) : SV_Position {
-    float3 position = positions[vertexIndex];
+    float3 position = positions[vertexIndex % 6];
     VertexShaderOutput output;
     output.position = float4(position, 1.0);
     output.uv = float2(position.x, position.y) * 0.5 + 0.5;
