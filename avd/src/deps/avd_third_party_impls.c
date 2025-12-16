19da76496375eaee4b1cd20320acea5757a95b6a
Jaysmito101 <jaysmito101@gmail.com>
2025-12-17 01:33:37 +0530

    feat: add debugging log macro for picoM3U8 + picothreads to compile deps

--- avd/src/deps/avd_third_party_impls.c
+++ avd/src/deps/avd_third_party_impls.c
@@ -13,6 +13,16 @@
 #define TINYOBJ_LOADER_C_IMPLEMENTATION
 #include "tinyobj_loader_c.h"
 
+#ifdef AVD_DEBUG
+// This one is purely for the purpose of debugging (using DebugPrint functions of picoM3U8)
+#define PICO_M3U8_LOG(...) do { \
+    printf(__VA_ARGS__); \
+    printf("\n"); \
+ } while(0)
+
+#endif
+
 #define PICO_IMPLEMENTATION
 #include "pico/picoLog.h"
 #include "pico/picoM3U8.h"
+#include "pico/picoThreads.h"
