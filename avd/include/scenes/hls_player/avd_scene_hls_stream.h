9df54b2196275a3de5c2967d77c1ea3c8b5f1c88
Jaysmito101 <jaysmito101@gmail.com>
2026-01-28 01:38:34 +0530

    feat: reimplement a better hls stream

--- avd/include/scenes/hls_player/avd_scene_hls_stream.h
+++ avd/include/scenes/hls_player/avd_scene_hls_stream.h
@@ -6,8 +6,9 @@
 #include "pico/picoStream.h"
 
 
-#define AVD_HLS_STREAM_INITIAL_CAPACITY (1024 * 64)
+#define AVD_HLS_STREAM_INITIAL_CAPACITY 1024 * 64
 #define AVD_HLS_STREAM_GROWTH_FACTOR    2
+#define AVD_HLS_STREAM_SEEK_BUFFER_SIZE   4096 // we shold be able to seek within this many bytes backwards or forwards at any time
 
 picoStream avdHLSStreamCreate();
 bool avdHLSStreamAppendData(picoStream stream, const AVD_UInt8 *data, AVD_Size dataSize);
--- avd/src/scenes/hls_player/avd_scenes_hls_player_stream.c
+++ avd/src/scenes/hls_player/avd_scenes_hls_player_stream.c
@@ -1,170 +1,117 @@
+#include "core/avd_types.h"
+#include "pico/picoStream.h"
 #include "scenes/hls_player/avd_scene_hls_stream.h"
 
 #include "core/avd_base.h"
 
-#include <string.h>
-
-typedef struct AVD_HLSStream {
-    AVD_UInt8 *buffer;
+typedef struct {
+    uint8_t *buffer;
     AVD_Size capacity;
-    AVD_Size writePos;
-    AVD_Size readPos;
-    AVD_Size dataSize;
-    picoStreamCustom_t customStream;
-} AVD_HLSStream;
-
-#define AVD_HLS_STREAM_FROM_CUSTOM(customPtr) \
-    ((AVD_HLSStream *)((char *)(customPtr) - offsetof(AVD_HLSStream, customStream)))
-
-static AVD_HLSStream *__avdHLSStreamFromPicoStream(picoStream stream)
-{
-    return (AVD_HLSStream *)picoStreamGetUserData(stream);
-}
-
-static bool __avdHLSStreamEnsureCapacity(AVD_HLSStream *hlsStream, AVD_Size requiredSize)
-{
-    if (requiredSize <= hlsStream->capacity) {
-        return true;
-    }
-
-    AVD_Size newCapacity = hlsStream->capacity;
-    while (newCapacity < requiredSize) {
-        newCapacity *= AVD_HLS_STREAM_GROWTH_FACTOR;
-    }
-
-    AVD_UInt8 *newBuffer = (AVD_UInt8 *)AVD_MALLOC(newCapacity);
-    AVD_CHECK_MSG(newBuffer != NULL, "Failed to allocate HLS stream buffer");
-
-    if (hlsStream->dataSize > 0) {
-        if (hlsStream->readPos + hlsStream->dataSize <= hlsStream->capacity) {
-            memcpy(newBuffer, hlsStream->buffer + hlsStream->readPos, hlsStream->dataSize);
-        } else {
-            AVD_Size firstPart = hlsStream->capacity - hlsStream->readPos;
-            memcpy(newBuffer, hlsStream->buffer + hlsStream->readPos, firstPart);
-            memcpy(newBuffer + firstPart, hlsStream->buffer, hlsStream->dataSize - firstPart);
-        }
-    }
-
-    AVD_FREE(hlsStream->buffer);
-    hlsStream->buffer   = newBuffer;
-    hlsStream->capacity = newCapacity;
-    hlsStream->readPos  = 0;
-    hlsStream->writePos = hlsStream->dataSize;
-
-    return true;
-}
+    AVD_Size readHead;
+    AVD_Size writeHead;
+    AVD_Int64 headOffset;
+} AVD_ScenesHLSStream;
 
 static size_t __avdHLSStreamRead(void *userData, void *buffer, size_t size)
 {
-    AVD_HLSStream *hlsStream = (AVD_HLSStream *)userData;
-    AVD_Size available       = hlsStream->dataSize;
-    AVD_Size toRead          = AVD_MIN(size, available);
+    AVD_ScenesHLSStream *hlsStream = (AVD_ScenesHLSStream *)userData;
+    AVD_ASSERT(hlsStream != NULL);
+    AVD_ASSERT(buffer != NULL || size == 0);
 
-    if (toRead == 0) {
+    AVD_Size available = hlsStream->writeHead - hlsStream->readHead;
+    size               = AVD_MIN(size, available);
+    if (size == 0) {
         return 0;
     }
 
-    AVD_UInt8 *dst = (AVD_UInt8 *)buffer;
+    memcpy(buffer, hlsStream->buffer + hlsStream->readHead, size);
+    hlsStream->readHead += size;
 
-    if (hlsStream->readPos + toRead <= hlsStream->capacity) {
-        memcpy(dst, hlsStream->buffer + hlsStream->readPos, toRead);
-    } else {
-        AVD_Size firstPart = hlsStream->capacity - hlsStream->readPos;
-        memcpy(dst, hlsStream->buffer + hlsStream->readPos, firstPart);
-        memcpy(dst + firstPart, hlsStream->buffer, toRead - firstPart);
-    }
-
-    hlsStream->readPos = (hlsStream->readPos + toRead) % hlsStream->capacity;
-    hlsStream->dataSize -= toRead;
-
-    return toRead;
+    return size;
 }
 
 static int __avdHLSStreamSeek(void *userData, int64_t offset, picoStreamSeekOrigin origin)
 {
-    AVD_HLSStream *hlsStream   = (AVD_HLSStream *)userData;
-    AVD_Size basePos           = (hlsStream->writePos >= hlsStream->dataSize)
-                                     ? (hlsStream->writePos - hlsStream->dataSize)
-                                     : (hlsStream->capacity - (hlsStream->dataSize - hlsStream->writePos));
-    AVD_Size currentReadOffset = (hlsStream->readPos >= basePos)
-                                     ? (hlsStream->readPos - basePos)
-                                     : (hlsStream->capacity - basePos + hlsStream->readPos);
-    int64_t newPos             = 0;
+    AVD_ScenesHLSStream *hlsStream = (AVD_ScenesHLSStream *)userData;
+    AVD_ASSERT(hlsStream != NULL);
 
     switch (origin) {
-        case PICO_STREAM_SEEK_SET:
-            newPos = offset;
+        case PICO_STREAM_SEEK_SET: {
+            if (offset < hlsStream->headOffset) {
+                // headOffset is the absolute position of the start of the buffer
+                // cannot seek before that
+                return -1;
+            }
+
+            if (offset - (AVD_Int64)hlsStream->headOffset > (AVD_Int64)hlsStream->writeHead) {
+                // cannot seek beyond write head
+                return -1;
+            }
+
+            hlsStream->readHead = (AVD_Size)(offset - (AVD_Int64)hlsStream->headOffset);
             break;
-        case PICO_STREAM_SEEK_CUR:
-            newPos = (int64_t)currentReadOffset + offset;
+        }
+        case PICO_STREAM_SEEK_CUR: {
+            AVD_Int64 targetPos = (AVD_Int64)hlsStream->readHead + offset;
+            if (targetPos < (AVD_Int64)0 || targetPos > (AVD_Int64)hlsStream->writeHead) {
+                return -1;
+            }
+
+            hlsStream->readHead = (AVD_Size)targetPos;
             break;
-        case PICO_STREAM_SEEK_END:
-            newPos = (int64_t)hlsStream->dataSize + offset;
-            break;
+        }
+        case PICO_STREAM_SEEK_END: {
+            AVD_LOG_WARN("Seeking from end not supported in HLS stream");
+            return -1;
+        }
         default:
             return -1;
     }
-
-    if (newPos < 0 || newPos > (int64_t)hlsStream->dataSize) {
-        return -1;
-    }
-
-    hlsStream->readPos = (basePos + (AVD_Size)newPos) % hlsStream->capacity;
 
     return 0;
 }
 
 static int64_t __avdHLSStreamTell(void *userData)
 {
-    AVD_HLSStream *hlsStream   = (AVD_HLSStream *)userData;
-    AVD_Size basePos           = (hlsStream->writePos >= hlsStream->dataSize)
-                                     ? (hlsStream->writePos - hlsStream->dataSize)
-                                     : (hlsStream->capacity - (hlsStream->dataSize - hlsStream->writePos));
-    AVD_Size currentReadOffset = (hlsStream->readPos >= basePos)
-                                     ? (hlsStream->readPos - basePos)
-                                     : (hlsStream->capacity - basePos + hlsStream->readPos);
-    return (int64_t)currentReadOffset;
-}
-
-static void __avdHLSStreamFlush(void *userData)
-{
-    (void)userData;
+    AVD_ScenesHLSStream *hlsStream = (AVD_ScenesHLSStream *)userData;
+    AVD_ASSERT(hlsStream != NULL);
+    return hlsStream->headOffset + (int64_t)hlsStream->readHead;
 }
 
 static void __avdHLSStreamDestroy(void *userData)
 {
-    AVD_HLSStream *hlsStream = (AVD_HLSStream *)userData;
+    AVD_ScenesHLSStream *hlsStream = (AVD_ScenesHLSStream *)userData;
+    AVD_ASSERT(hlsStream != NULL);
     if (hlsStream->buffer) {
         AVD_FREE(hlsStream->buffer);
+        hlsStream->buffer = NULL;
     }
     AVD_FREE(hlsStream);
 }
 
 picoStream avdHLSStreamCreate()
 {
-    AVD_Size initialCapacity = AVD_HLS_STREAM_INITIAL_CAPACITY;
-    AVD_HLSStream *hlsStream = (AVD_HLSStream *)AVD_MALLOC(sizeof(AVD_HLSStream));
-    AVD_ASSERT(hlsStream != NULL);
+    picoStreamCustom_t customStream = {0};
+    AVD_ScenesHLSStream *hlsStream  = (AVD_ScenesHLSStream *)AVD_MALLOC(sizeof(AVD_ScenesHLSStream));
+    if (!hlsStream) {
+        return NULL;
+    }
 
-    memset(hlsStream, 0, sizeof(AVD_HLSStream));
+    memset(hlsStream, 0, sizeof(AVD_ScenesHLSStream));
+    hlsStream->capacity = AVD_HLS_STREAM_INITIAL_CAPACITY;
+    hlsStream->buffer   = (uint8_t *)AVD_MALLOC(hlsStream->capacity);
+    if (!hlsStream->buffer) {
+        AVD_FREE(hlsStream);
+        return NULL;
+    }
 
-    hlsStream->buffer = (AVD_UInt8 *)AVD_MALLOC(initialCapacity);
-    AVD_ASSERT(hlsStream->buffer != NULL);
+    customStream.userData = hlsStream;
+    customStream.read     = __avdHLSStreamRead;
+    customStream.seek     = __avdHLSStreamSeek;
+    customStream.tell     = __avdHLSStreamTell;
+    customStream.destroy  = __avdHLSStreamDestroy;
 
-    hlsStream->capacity = initialCapacity;
-    hlsStream->writePos = 0;
-    hlsStream->readPos  = 0;
-    hlsStream->dataSize = 0;
-
-    hlsStream->customStream.userData = hlsStream;
-    hlsStream->customStream.read     = __avdHLSStreamRead;
-    hlsStream->customStream.write    = NULL;
-    hlsStream->customStream.seek     = __avdHLSStreamSeek;
-    hlsStream->customStream.tell     = __avdHLSStreamTell;
-    hlsStream->customStream.flush    = __avdHLSStreamFlush;
-    hlsStream->customStream.destroy  = __avdHLSStreamDestroy;
-
-    return picoStreamFromCustom(&hlsStream->customStream, true, false);
+    return picoStreamFromCustom(customStream, true, false);
 }
 
 bool avdHLSStreamAppendData(picoStream stream, const AVD_UInt8 *data, AVD_Size dataSize)
@@ -172,27 +119,46 @@ bool avdHLSStreamAppendData(picoStream stream, const AVD_UInt8 *data, AVD_Size d
     AVD_ASSERT(stream != NULL);
     AVD_ASSERT(data != NULL || dataSize == 0);
 
-    if (dataSize == 0) {
-        return true;
-    }
-
-    AVD_HLSStream *hlsStream = __avdHLSStreamFromPicoStream(stream);
+    AVD_ScenesHLSStream *hlsStream = (AVD_ScenesHLSStream *)picoStreamGetUserData(stream);
     AVD_ASSERT(hlsStream != NULL);
 
-    if (!__avdHLSStreamEnsureCapacity(hlsStream, hlsStream->dataSize + dataSize)) {
-        return false;
+    // check if there is enough room in the buffer
+    AVD_Size currentSize   = hlsStream->writeHead - hlsStream->readHead + AVD_HLS_STREAM_SEEK_BUFFER_SIZE;
+    AVD_Size availableSize = hlsStream->capacity - currentSize;
+    AVD_Int64 startOffset  = AVD_MAX(0, (AVD_Int64)hlsStream->readHead - AVD_HLS_STREAM_SEEK_BUFFER_SIZE);
+    AVD_Size moveSize      = hlsStream->writeHead - (AVD_Size)startOffset;
+
+    if (dataSize > availableSize) {
+        // need to grow the buffer
+        AVD_Size newCapacity = hlsStream->capacity;
+        while (dataSize > (newCapacity - currentSize)) {
+            newCapacity *= AVD_HLS_STREAM_GROWTH_FACTOR;
+        }
+
+        uint8_t *newBuffer = (uint8_t *)AVD_MALLOC(newCapacity);
+        if (!newBuffer) {
+            AVD_LOG_ERROR("Failed to allocate memory for HLS stream growth");
+            return false;
+        }
+
+        // copy existing data to new buffer
+        memcpy(newBuffer, hlsStream->buffer + (AVD_Size)startOffset, moveSize);
+        AVD_FREE(hlsStream->buffer);
+
+        hlsStream->buffer   = newBuffer;
+        hlsStream->capacity = newCapacity;
+    } else if (startOffset > 0) {
+        // move the readHead towards the beginning to make room
+        memmove(hlsStream->buffer, hlsStream->buffer + (AVD_Size)startOffset, moveSize);
     }
 
-    if (hlsStream->writePos + dataSize <= hlsStream->capacity) {
-        memcpy(hlsStream->buffer + hlsStream->writePos, data, dataSize);
-    } else {
-        AVD_Size firstPart = hlsStream->capacity - hlsStream->writePos;
-        memcpy(hlsStream->buffer + hlsStream->writePos, data, firstPart);
-        memcpy(hlsStream->buffer, data + firstPart, dataSize - firstPart);
-    }
+    hlsStream->headOffset += (AVD_Int64)startOffset;
+    hlsStream->readHead -= (AVD_Size)startOffset;
+    hlsStream->writeHead -= (AVD_Size)startOffset;
 
-    hlsStream->writePos = (hlsStream->writePos + dataSize) % hlsStream->capacity;
-    hlsStream->dataSize += dataSize;
+    // append new data
+    memcpy(hlsStream->buffer + hlsStream->writeHead, data, dataSize);
+    hlsStream->writeHead += dataSize;
 
     return true;
 }
