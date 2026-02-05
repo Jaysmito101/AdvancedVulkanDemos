9a47b7e8c87c16abdb5fecc051c6a61aebe81814
Jaysmito101 <jaysmito101@gmail.com>
2026-02-05 07:10:47 +0530

    fix: update mouse delta calculation to use avdInputCalculateMouseDeltas

--- avd/src/core/avd_window_cb.c
+++ avd/src/core/avd_window_cb.c
@@ -67,6 +67,7 @@ void __avdGLFWCursorPosCallback(GLFWwindow *window, double xpos, double ypos)
     event.type        = AVD_INPUT_EVENT_MOUSE_MOVE;
     event.mouseMove.x = appState->input.mouseX;
     event.mouseMove.y = appState->input.mouseY;
+    avdInputCalculateMouseDeltas(&appState->input);
     avdSceneManagerPushInputEvent(&appState->sceneManager, appState, &event);
 }
 
@@ -105,7 +106,7 @@ void __avdGLFWWindowSizeCallback(GLFWwindow *window, int width, int height)
     avdSceneManagerPushInputEvent(&appState->sceneManager, appState, &event);
 
     appState->swapchain.swapchainRecreateRequired = true;
-    avdInputCalculateDeltas(&appState->input);
+    avdInputCalculateMouseDeltas(&appState->input);
     avdApplicationUpdateWithoutPolling(appState);
 }
 
